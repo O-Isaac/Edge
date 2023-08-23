e30ca74672eaf2f8d949f914b3123d0fc45316f8
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-08-24 00:04:48 +0200

    fix(next-sitemap.config.js): prevent url preview present in production

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://edge-ui-docs.vercel.app/',
+    siteUrl: process.env.VERCEL_ENV === 'preview' ? `https://${process.env.VERCEL_URL}` : 'https://edge-ui-docs.vercel.app/',
     generateRobotsTxt: true,
 }
   
