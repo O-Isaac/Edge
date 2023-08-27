6436c103aa24d123a38b41e30602858374869ef9
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-08-27 19:11:30 +0200

    fix(Hero.jsx): Fix z-index menu in mobile

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 function Hero() {
   return (
-    <section className="screen p-4 grid text-center place-items-center place-content-center relative">
-      <h1 className="relative text-7xl lg:text-8xl mb-6 font-medium z-30">Edge UI</h1>
-      <p className="relative text-3xl lg:text-4xl font-light z-30">Ultimate Interface Overhaul</p>
-      <div className="relative grid p-4 mt-2 transition-all z-30">
+    <section className="relative grid p-4 text-center screen place-items-center place-content-center">
+      <h1 className="relative z-10 mb-6 font-medium text-7xl lg:text-8xl">Edge UI</h1>
+      <p className="relative z-10 text-3xl font-light lg:text-4xl">Ultimate Interface Overhaul</p>
+      <div className="relative z-10 grid p-4 mt-2 transition-all">
         <Link
-          className="px-4 py-3 rounded-lg  border hover:bg-zinc-50 dark:hover:text-black transition-all"
+          className="px-4 py-3 transition-all border rounded-lg hover:bg-zinc-50 dark:hover:text-black"
           href="documentation"
         >
           Read Documentation
