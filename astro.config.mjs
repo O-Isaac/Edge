8551bda1e1e6e76e951124b3caa368db7e03626e
O-Isaac <soporte@proyectograndorder.es>
2024-10-11 18:56:10 +0200

    Remove Site Map Startlight support sitemap generate then is not necesary

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,40 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import metatags from './meta.config.mjs';
-
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import metatags from "./meta.config.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://beta-edge-ui-docs.vercel.app",
-  integrations: [starlight({
-    title: 'Docs',
-    head: metatags,
-    logo: {
-      src: "./src/assets/logo.webp"
-    },
-    social: {
-      discord: 'https://discord.gg/5stwNU5M',
-      patreon: 'https://www.patreon.com/EdgeUI'
-    },
-    favicon: "/favicon.png",
-    customCss: ['./src/styles/custom.css'],
-    sidebar: [{
-      label: "Guides",
-      autogenerate: {
-        directory: "guides"
-      }
-    }, {
-      label: 'Mods',
-      autogenerate: {
-        directory: "mods"
-      }
-    }],
-    components: {
-      Head: "./src/components/HeadViewTransition.astro"
-    }
-  }), sitemap()]
+  site: "https://edge-ui-docs.vercel.app",
+  integrations: [
+    starlight({
+      title: "Docs",
+      head: metatags,
+      logo: {
+        src: "./src/assets/logo.webp",
+      },
+      social: {
+        discord: "https://discord.gg/5stwNU5M",
+        patreon: "https://www.patreon.com/EdgeUI",
+      },
+      favicon: "/favicon.png",
+      customCss: ["./src/styles/custom.css"],
+      sidebar: [
+        {
+          label: "Guides",
+          autogenerate: {
+            directory: "guides",
+          },
+        },
+        {
+          label: "Mods",
+          autogenerate: {
+            directory: "mods",
+          },
+        },
+      ],
+      components: {
+        Head: "./src/components/HeadViewTransition.astro",
+      },
+    }),
+  ],
 });
