c5b8ced3eff6cadf222f7b7eb75c80e57ad4fcc6
O-Isaac <soporte@proyectograndorder.es>
2024-10-11 18:50:42 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,37 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
 import metatags from './meta.config.mjs';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://beta-edge-ui-docs.vercel.app",
-	integrations: [
-		starlight({
-			title: 'Docs',
-			head: metatags,
-			logo: {
-				src: "./src/assets/logo.webp"
-			},
-			social: {
-				discord: 'https://discord.gg/5stwNU5M',
-				patreon: 'https://www.patreon.com/EdgeUI'
-			},
-			favicon: "/favicon.png",
-			customCss: ['./src/styles/custom.css'],
-			sidebar: [
-				{
-					label: "Guides",
-					autogenerate: { directory: "guides" }
-				},
-				{
-					label: 'Mods',
-					autogenerate: { directory: "mods" }
-				},
-			],
-			components: {
-				Head: "./src/components/HeadViewTransition.astro",
-			}
-		}),
-	],
+  site: "https://beta-edge-ui-docs.vercel.app",
+  integrations: [starlight({
+    title: 'Docs',
+    head: metatags,
+    logo: {
+      src: "./src/assets/logo.webp"
+    },
+    social: {
+      discord: 'https://discord.gg/5stwNU5M',
+      patreon: 'https://www.patreon.com/EdgeUI'
+    },
+    favicon: "/favicon.png",
+    customCss: ['./src/styles/custom.css'],
+    sidebar: [{
+      label: "Guides",
+      autogenerate: {
+        directory: "guides"
+      }
+    }, {
+      label: 'Mods',
+      autogenerate: {
+        directory: "mods"
+      }
+    }],
+    components: {
+      Head: "./src/components/HeadViewTransition.astro"
+    }
+  }), sitemap()]
 });
